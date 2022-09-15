65a80179d883c0cfc834150630507577c278db34
ravi <ravitokas.9000@gmail.com>
2022-09-15 20:29:39 +0400

    Create a Server (Hello World) and other pages in Node & ExpressJS

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # mern-stack-pro
+
+1. create a link from server like About | Contact | Login | Sign Up
--- app.js
+++ app.js
@@ -2,8 +2,29 @@ const express = require("express");
 
 const app = express();
 
+// Default link
 app.get("/", (req, res) => {
   res.send(`Hello world from the server`);
+});
+
+// about link page
+app.get("/about", (req, res) => {
+  res.send(`About Page from the server`);
+});
+
+// contact link page
+app.get("/contact", (req, res) => {
+  res.send(`Contact page from the server`);
+});
+
+// login link page
+app.get("/login", (req, res) => {
+  res.send(`Login page from the server`);
+});
+
+// signUp link page
+app.get("/signup", (req, res) => {
+  res.send(`sign up page from the server`);
 });
 
 app.listen(3001, () => {

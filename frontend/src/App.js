9172ddd5e9e9aa6d91d0b266d490fd714ec2e9c4
ravi <ravitokas.9000@gmail.com>
2022-09-20 00:57:38 +0400

    Create Responsive NAVBAR (MENU)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 //import logo from "./logo.svg";
 import "./App.css";
-
+import Navbar from "./components/Navbar";
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <Navbar />
+      </header>
       <h1>Hello World</h1>
     </div>
   );
